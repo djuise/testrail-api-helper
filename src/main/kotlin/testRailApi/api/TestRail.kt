a63981b06b5ccc8050a69c2991981d6864a27f0e
Yevhenii Blynnyi <yevhenii.blynnyi@gen.tech>
2024-08-29 22:46:00 +0300

    pom updated

--- src/main/kotlin/testRailApi/api/TestRail.kt
+++ src/main/kotlin/testRailApi/api/TestRail.kt
@@ -2,7 +2,7 @@ package testRailApi.api
 
 import okhttp3.Credentials
 
-class TestRail(url: String, private val username: String, private val password: String) {
+public class TestRail(url: String, private val username: String, private val password: String) {
 
     constructor(url: String, apiToken: String): this(url, apiToken, String())
 
