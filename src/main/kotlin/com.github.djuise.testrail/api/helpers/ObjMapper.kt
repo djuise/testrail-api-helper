499c34a8000fa962d2b27755c0db30656580ba55
Yevhenii Blynnyi <yevhenii.blynnyi@gen.tech>
2024-12-10 13:43:24 +0200

    2.0.2 fix run creation

--- src/main/kotlin/com.github.djuise.testrail/api/helpers/ObjMapper.kt
+++ src/main/kotlin/com.github.djuise.testrail/api/helpers/ObjMapper.kt
@@ -2,9 +2,15 @@ package com.github.djuise.testrail.api.helpers
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 
 val objectMapper = ObjectMapper()
     .registerModule(KotlinModule.Builder().build())
 //    .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+val jsonMapper = ObjectMapper()
+    .registerModule(KotlinModule.Builder().build())
+    .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
--- src/main/kotlin/com.github.djuise.testrail/api/requests/Run.kt
+++ src/main/kotlin/com.github.djuise.testrail/api/requests/Run.kt
@@ -4,12 +4,12 @@ import com.github.djuise.testrail.api.TestRailRequest
 import com.github.djuise.testrail.api.dto.CreateRunDTO
 import com.github.djuise.testrail.api.dto.RunDTO
 import com.github.djuise.testrail.api.helpers.Constants.API2
-import com.github.djuise.testrail.api.helpers.objectMapper
+import com.github.djuise.testrail.api.helpers.jsonMapper
 
 object Run {
 
     fun create(projectId: Int, run: CreateRunDTO): Int {
-        val runJson = objectMapper.writeValueAsString(run)
+        val runJson = jsonMapper.writeValueAsString(run)
         val response = TestRailRequest.post("$API2/add_run/$projectId", runJson, RunDTO::class.java)
 
         return response.id
