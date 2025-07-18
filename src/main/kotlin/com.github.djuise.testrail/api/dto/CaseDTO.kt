2662dcea78b8be156554fabfed7d529155fdf469
Yevhenii Blynnyi <yevhenii.blynnyi@gen.tech>
2025-07-18 21:53:48 +0300

    3.0.4 - added vars to CaseDTO

--- src/main/kotlin/com.github.djuise.testrail/api/dto/CaseDTO.kt
+++ src/main/kotlin/com.github.djuise.testrail/api/dto/CaseDTO.kt
@@ -8,30 +8,30 @@ import com.github.djuise.testrail.api.helpers.jsonMapper
 import com.github.djuise.testrail.api.helpers.objectMapper
 import java.util.*
 
-data class CasesDTO(var cases: List<CaseDTO>)
-data class UpdatedCasesDTO(var updatedCases: List<CaseDTO>)
+data class CasesDTO(val cases: List<CaseDTO>)
+data class UpdatedCasesDTO(val updatedCases: List<CaseDTO>)
 
 data class CaseDTO(
     val id: Int,
-    val title: String,
-    val createdBy: Int,
+    var title: String,
+    var createdBy: Int,
     @JsonDeserialize(using = UnixTimestampDeserializer::class)
-    val createdOn: Date,
-    val estimate: String?,
-    val estimateForecast: String?,
-    val milestoneId: Int?,
-    val priorityId: Int?,
-    val refs: String?,
-    val sectionId: Int?,
-    val suiteId: Int,
-    val templateId: Int?,
-    val typeId: Int?,
+    var createdOn: Date,
+    var estimate: String?,
+    var estimateForecast: String?,
+    var milestoneId: Int?,
+    var priorityId: Int?,
+    var refs: String?,
+    var sectionId: Int?,
+    var suiteId: Int,
+    var templateId: Int?,
+    var typeId: Int?,
     val updatedBy: Int?,
     @JsonDeserialize(using = UnixTimestampDeserializer::class)
     val updatedOn: Date?,
     val isDeleted: Int?,
     val displayOrder: Int?,
-    val otherFields: MutableMap<String, Any?> = mutableMapOf()
+    var otherFields: MutableMap<String, Any?> = mutableMapOf()
 ) {
     @JsonAnySetter
     fun setDynamicField(key: String, value: Any?) {
